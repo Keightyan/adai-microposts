19d7c59da5c38cbfeddc2420856d72a05c7d4a38
Keight <ke169861@gmail.com>
2019-11-23 13:37:44 +0000

    Procfile

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -6,4 +6,4 @@
         <img class="rounded img-fluid" src="{{ Gravatar::src($user->email, 500) }}" alt="">
     </div>
 </div>
-@include('user_follow.follow_button', ['user' => $user])
+@include('user_follow.follow_button', ['user' => $user])　<!-- ← 第2引数の ['user' => $user] は省略可（同一viewファイルに $user の記述があるため -->
